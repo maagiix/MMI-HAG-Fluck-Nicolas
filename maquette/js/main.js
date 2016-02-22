363a8f1308cf1d4852a4b88ef3a54e587c7c31f1
Nicolas FLUCK <nicolas@fluck.us>
2016-02-22 08:52:44 +0100

    update 22/02

--- maquette/js/main.js
+++ maquette/js/main.js
@@ -1,81 +1,50 @@
 $(document).ready(function() {
-   var trait = $('hr');
    var brand1 = $('.brand1');
    var brand2 = $('.brand2');
    var brand3 = $('.brand3');
    var brand4 = $('.brand4');
    var brand5 = $('.brand5');
+   var trait = $(this).children(".trait hr");
    var textBrand = $('.text-brand');
+   var bulle = $('.bulle4');
 
+   brand1.hover(function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
 
-   brand1.on("mouseover", function(){
-
-   		brand1.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
+   }, function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #000');
    });
 
-   brand2.on('mouseover', function(){
-   		brand2.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
+   brand2.hover(function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
 
-   brand3.on('mouseover', function(){
-   		brand3.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand4.on('mouseover', function(){
-   		brand4.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand5.on('mouseover', function(){
-   		brand5.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
+   }, function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #000');
    });
 
 
+brand3.hover(function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
 
-
-   brand1.on("mouseleave", function(){
-
-   		brand1.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand2.on('mouseleave', function(){
-   		brand2.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand3.on('mouseleave', function(){
-   		brand3.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand4.on('mouseleave', function(){
-   		brand4.css( "cursor", "pointer" );
-   		trait.css("border", "3px solid #57c5a0");
-   		textBrand.css("color", "#FFF");
-   });
-
-   brand5.on('mouseleave', function(){
-   		trait.css("border", "3px solid #000");
-   		textBrand.css("color", "#979797");
+   }, function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #000');
    });
 
 
+brand4.hover(function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
+
+   }, function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #000');
+   });
 
 
+brand5.hover(function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
 
+   }, function() {
+      $(this).children(".trait").children("hr").css('border', '3px solid #000');
+   });
 
 
 
