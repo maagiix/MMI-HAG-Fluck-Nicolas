bde0658610aec33f6936af41f498aa61b43d8a98
Nicolas FLUCK <nicolas@fluck.us>
2016-02-25 09:27:01 +0100

    Commit final
    FLEXBOX: utilisation sur les liens du slider, les arrivals, les best
    sellers et la div "On sale, special offers, must have".
    
    PERFORMANCE: utilisation de font-awesome pour les icones du slider et
    des réseaux sociaux.
    
    TRANSITION: utilisation des transition pour les boutons "show all" ainsi
    qu'au hover d'une chaussure.

--- maquette/js/main.js
+++ maquette/js/main.js
@@ -1,55 +1,13 @@
 $(document).ready(function() {
-   var brand1 = $('.brand1');
-   var brand2 = $('.brand2');
-   var brand3 = $('.brand3');
-   var brand4 = $('.brand4');
-   var brand5 = $('.brand5');
-   var trait = $(this).children(".trait hr");
-   var textBrand = $('.text-brand');
-   var bulle = $('.bulle4');
-
-   brand1.hover(function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
-
-   }, function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #000');
+   $('.item1').on('click', function(){
+      $('.key').children.deleteClass('bouncing');
+      $('.key').children.addClass('bounceRight');
+      $('.key').children.deleteClass('bounceTop');
    });
 
-   brand2.hover(function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
-
-   }, function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #000');
+    $('.item2').on('click', function(){
+    	$('.key').children.deleteClass('bouncing');
+      $('key').children.addClass('bounceTop');
+      $('.key').children.deleteClass('bounceRight');
    });
-
-
-brand3.hover(function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
-
-   }, function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #000');
-   });
-
-
-brand4.hover(function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
-
-   }, function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #000');
-   });
-
-
-brand5.hover(function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #57c5a0');
-
-   }, function() {
-      $(this).children(".trait").children("hr").css('border', '3px solid #000');
-   });
-
-
-
-
-
-
-
 });
